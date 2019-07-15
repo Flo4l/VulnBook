d7752a3414b9e339c647cb69305baa8d49d511d3
Florian Albers <Florian.Albers@adesso.de>
2019-07-15 10:02:52 +0200

    Initial commit

---
+++ src/main/java/de/albers/vulnbook/VulnbookApplication.java
@@ -0,0 +1,13 @@
+package de.albers.vulnbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VulnbookApplication.class, args);
+    }
+
+}
