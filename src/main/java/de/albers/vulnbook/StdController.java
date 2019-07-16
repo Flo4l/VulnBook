84e0586c116ef40ae07b8115e5d2d028c127198b
Florian Albers <Florian.Albers@adesso.de>
2019-07-16 07:47:21 +0200

    Added pages: home, login, register

--- src/main/java/de/albers/vulnbook/StdController.java
+++ src/main/java/de/albers/vulnbook/StdController.java
@@ -7,8 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StdController {
 
-    @GetMapping("/")
-    public String homePage(Model model) {
-        return "sites/index.html";
+    @GetMapping("/feed")
+    public String feedPage(Model model) {
+        return "sites/feed.html";
     }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        return "sites/login.html";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        return "sites/register.html";
+    }
+
+    @GetMapping({"/home", "/"})
+    public String homePage(Model model) {
+        return "sites/home.html";
+    }
+
 }
